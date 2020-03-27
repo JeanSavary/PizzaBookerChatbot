9722171c18a056d2bc0baca2f95584f60994af90
JeanSavary <jean.savary@imt-atlantique.net>
2020-03-27 19:24:52 +0100

    Modifications faites par Nicolas :modi getPizzawithIngred et ajout de l'intent getPizzaComposition

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-# import flask dependencies
 from flask import Flask, request, make_response, jsonify
 import pandas as pd 
 import numpy as np
 from copy import deepcopy
-import Util
-
 
 # initialize the flask app
 app = Flask(__name__)
 DATA = pd.read_csv('data/pizzas.csv', sep = ';')
 
 # To move into another file within a class
-
-
 def creation_df_bool_presence(col,list_elements, df, conjonction='addition'):
     """ This function creates a dataFrame with booleans, True if the element (string) is inthe column col (string) of the dataframe d, else false
     It is used for to check is ingredients are within a pizza or not for example """
     nb_element = len(list_elements)
     df_temp = pd.DataFrame(index=df.index)
     for element in list_elements:
-        df_temp[element]=df[col].apply(lambda x: True if element in x else False)
+        if col=='ingredients':  #because in the csv the first letter is upper, it doesn't apply for the case it s pizza
+            element = element.lower()
+            element = element[0].upper()+element[1:] 
+        elif col=='name' : #case it s GetPizzaComposition
+            if ' ' in element:
+                element = element.split(' ')[-1] #case it s 'pizza chipo', we keep just 'chipo'
+            element = element[0].upper()+element[1:]
+        df_temp[element]=df[col].apply(lambda x: True if (element in x or element[:-1] in x) else False) #element[:-1] for the case it s a plurial in the question
     df_temp['sum'] = df_temp.sum(axis=1)
     if conjonction =='addition':
         df_return = pd.DataFrame(df_temp['sum'].apply(lambda x:True if x==nb_element else False)) #all the ingredients are needed to select the pizza
@@ -44,9 +46,10 @@ def pizza_without_ingredient(self, ingredient, DATA):
     return(list_pizza)
 
 def format_list_for_message_client(list_data):
-    list_data_string = list_data.replace('[','')
+    list_data_string = str(list_data).replace('[','')
     list_data_string = list_data_string.replace(']','')
     list_data_string = list_data_string.replace('\'','')
+    list_data_string = list_data_string.replace('\"','')
     return (str(list_data_string))
 
 # function for responses
@@ -54,107 +57,128 @@ def results():
     # build a request object
     req = request.get_json(force=True)
 
+    if req.get('queryResult').get('intent').get('displayName') == 'GetPizzaComposition':
+        pizza = req.get('queryResult').get('outputContexts')[0].get('parameters').get('pizza-type.original')
+        print("pizzaaa", type(pizza), pizza)
+        df_bool = creation_df_bool_presence('name', pizza, DATA )
+        print(df_bool)
+        if len(df_bool['sum'].unique())==1: #only false, so the name of the pizza doesn t exist
+            return {'fulfillmentText': u'Nous sommes désolé, n\'avons pas cette pizza.'}
+        elif len(df_bool['sum'].unique())==2: #there are True and False so the pizza is in the menu
+            index_pizza = df_bool.loc[df_bool['sum']==True].index[0]
+            print("index", index_pizza)
+            str_ingredients = DATA.loc[index_pizza, "ingredients"]
+            return {'fulfillmentText': u'La {} contient les ingrédients {}.'.format(format_list_for_message_client(pizza),str_ingredients)}
+        
+
+
+
+
     if req.get('queryResult').get('intent').get('displayName') == 'GetPizzaWithIngredients':  #If the client ask for a pizza which contains a list of ingredients
-        is_remover = req.get('queryResult').get('parameters').get('ingredient-modification')   # several values ('avec', 'sans', 'enlever', 'ajouter') just the two first values are useful here
+        is_remover = req.get('queryResult').get('parameters').get('ingredient-modification')[0]   # several values ('avec', 'sans', 'enlever', 'ajouter') just the two first values are useful here
         #ingredients_ = req.get('queryResult').get('parameters').get('ingredients')  #list of string, required entity
-        ingredients = req.get('queryResult').get('outputContexts').get('ingredients.original') #list of string
+        ingredients = req.get('queryResult').get('outputContexts')[0].get('parameters').get('ingredients.original')
         quantity = req.get('queryResult').get('parameters').get('quantity') #string, not required entity, possible values : 'singulier', 'pluriel'
         list_pizza = []  # list of pizzas which will match to the request (with/without ingredients)
-        print(ingredients)
-
+        print('ok ça tourne', ingredients, quantity)
+        if len(quantity)==0:  # case the quantity hasn't been tagged by dialogflow, it is set to plurial automatically
+            quantity = ["pluriel"]
+        print(quantity)
         if is_remover=='avec' : 
+            
             if len(ingredients)==1: #just for one ingredient
 
                 df_bool = creation_df_bool_presence('ingredients',ingredients, DATA)
-                list_pizza = select_bool_column(df_bool, DATA, 'ingredients', True) # call the function which 
-
+                list_pizza = select_bool_column(df_bool, DATA, 'name', True) # call the function which 
+                print(list_pizza)
                 if len(list_pizza)==0 :
                     return {'fulfillmentText': u'Nous n\'avons pas de pizza avec l\'ingrédient demandé'}
-                elif quantity =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
+                elif quantity[0] =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
                     random_pizza = np.random.randint(0, len(list_pizza)) #random int to not always have the first pizzas returned 
+                    print("random", random_pizza)
                     return {'fulfillmentText': u'La {} contient l\'ingrédient {}.'.format(list_pizza[random_pizza], ingredients[0])}
 
-                elif len(list_pizza)==1 and quantity=='pluriel': #just one pizza whereas the client asks for several pizzas
+                elif len(list_pizza)==1 and quantity[0]=='pluriel': #just one pizza whereas the client asks for several pizzas
                     return {'fulfillmentText': u'Nous avons uniquement la pizza {} avec l\'ingrédient {}'.format(list_pizza[0], ingredients[0])}
-                elif quantity =='pluriel' and len(list_pizza)>=2:
+                elif quantity[0] =='pluriel' and len(list_pizza)>=2:
                     return {'fulfillmentText': u'Les pizzas avec l\'ingrédient {} sont {}'.format(ingredients[0], format_list_for_message_client(list_pizza))}
                 
             elif len(ingredients)>1: #there are several ingredients in the list
                 conjonction = req.get('queryResult').get('parameters').get('conjonction')
-                if conjonction == 'addition' :  #case 'je veux pizza avec salade ET tomate'
+                print('conjonction', conjonction)
+                if conjonction[0] == 'addition' :  #case 'je veux pizza avec salade ET tomate'
                     df_bool = creation_df_bool_presence('ingredients',ingredients, DATA)
-                    list_pizza = select_bool_column(df_bool, DATA, 'ingredients', True)
+                    list_pizza = select_bool_column(df_bool, DATA, 'name', True)
 
                     if len(list_pizza)==0 :
                         return {'fulfillmentText': u'Nous n\'avons pas de pizza avec les ingrédients demandés'}
-                    elif quantity =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
+                    elif quantity[0] =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
                         random_pizza = np.random.randint(0, len(list_pizza)) #random int to not always have the first pizzas returned 
                         return {'fulfillmentText': u'La {} contient les ingrédients {}.'.format(list_pizza[random_pizza], format_list_for_message_client(ingredients))}
 
-                    elif len(list_pizza)==1 and quantity=='pluriel': #just one pizza whereas the client asks for several pizzas
+                    elif len(list_pizza)==1 and quantity[0]=='pluriel': #just one pizza whereas the client asks for several pizzas
                         return {'fulfillmentText': u'Nous avons uniquement la pizza {} avec les ingrédients {}'.format(list_pizza[0], format_list_for_message_client(ingredients))}
-                    elif quantity =='pluriel' and len(list_pizza)>=2:
+                    elif quantity[0]=='pluriel' and len(list_pizza)>=2:
                         return {'fulfillmentText': u'Les pizzas avec l\'ingrédient {} sont {}'.format(format_list_for_message_client(ingredients), format_list_for_message_client(list_pizza))}
 
-                elif conjonction == 'ou':  #case 'je veux pizza avec salade OU tomates'
+                elif conjonction[0]== 'ou':  #case 'je veux pizza avec salade OU tomates'
                     df_bool = creation_df_bool_presence('ingredients',ingredients, DATA, conjonction='ou')
-                    list_pizza = select_bool_column(df_bool, DATA, 'ingredients', True)
+                    list_pizza = select_bool_column(df_bool, DATA, 'name', True)
 
                     if len(list_pizza)==0 :
                         return {'fulfillmentText': u'Nous n\'avons pas de pizza avec l\'un des ingrédients demandés'}
-                    elif quantity =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
+                    elif quantity[0]=='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
                         random_pizza = np.random.randint(0, len(list_pizza)) #random int to not always have the first pizzas returned 
                         return {'fulfillmentText': u'La {} contient au moins un des ingrédients {}.'.format(list_pizza[random_pizza], format_list_for_message_client(ingredients))}
 
-                    elif len(list_pizza)==1 and quantity=='pluriel': #just one pizza whereas the client asks for several pizzas
+                    elif len(list_pizza)==1 and quantity[0]=='pluriel': #just one pizza whereas the client asks for several pizzas
                         return {'fulfillmentText': u'Nous avons uniquement la pizza {} avec un des ingrédients {}'.format(list_pizza[0], format_list_for_message_client(ingredients))}
-                    elif quantity =='pluriel' and len(list_pizza)>=2:
+                    elif quantity[0]=='pluriel' and len(list_pizza)>=2:
                         return {'fulfillmentText': u'Les pizzas avec au moins un des ingrédients {} sont {}'.format(format_list_for_message_client(ingredients), format_list_for_message_client(list_pizza))}
 
         elif is_remover=='sans' :  #the client wants to know if there is pizza(s) without ingredient(s)
             if len(ingredients)==1: #just for one ingredient
 
                 df_bool = creation_df_bool_presence('ingredients',ingredients, DATA)
-                list_pizza = select_bool_column(df_bool, DATA, 'ingredients', False) # call the function which selects the rows(pizzas) where there isn't the ingredient
+                list_pizza = select_bool_column(df_bool, DATA, 'name', False) # call the function which selects the rows(pizzas) where there isn't the ingredient
 
                 if len(list_pizza)==0 :
                     return {'fulfillmentText': u'Nous n\'avons pas de pizza sans l\'ingrédient demandé'}
-                elif quantity =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza without the ingredient
+                elif quantity[0]=='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza without the ingredient
                     random_pizza = np.random.randint(0, len(list_pizza)) #random int to not always have the first pizzas returned 
                     return {'fulfillmentText': u'La {} ne contient pas l\'ingrédient {}.'.format(list_pizza[random_pizza], ingredients[0])}
 
-                elif len(list_pizza)==1 and quantity=='pluriel': #just one pizza whereas the client asks for several pizzas
+                elif len(list_pizza)==1 and quantity[0]=='pluriel': #just one pizza whereas the client asks for several pizzas
                     return {'fulfillmentText': u'Nous avons uniquement la pizza {} sans l\'ingrédient {}'.format(list_pizza[0], ingredients[0])}
-                elif quantity =='pluriel' and len(list_pizza)>=2:
+                elif quantity[0]=='pluriel' and len(list_pizza)>=2:
                     return {'fulfillmentText': u'Les pizzas sans l\'ingrédient {} sont {}'.format(ingredients[0], format_list_for_message_client(list_pizza))}
                 
             elif len(ingredients)>1: #there are several ingredients in the list
                 conjonction = req.get('queryResult').get('parameters').get('conjonction') 
                 # We supposed there is no possibility that the conjonction would be 'ou' : 'je veux des pizzas sans tomates ou poivrons'
-                if conjonction == 'addition' :  #case 'je veux pizza sans salade ET tomate'
-                    df_bool = creation_df_bool_presence('ingredients',ingredients, DATA)
-                    list_pizza = select_bool_column(df_bool, DATA, 'ingredients', False)
+                if conjonction[0] == 'addition' :  #case 'je veux pizza sans salade ET tomate'
 
+                    df_bool = creation_df_bool_presence('ingredients',ingredients, DATA)
+                    list_pizza = select_bool_column(df_bool, DATA, 'name', False)
                     if len(list_pizza)==0 :
                         return {'fulfillmentText': u'Nous n\'avons pas de pizza sans les ingrédients demandés'}
-                    elif quantity =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
+                    elif quantity[0] =='singulier' and len(list_pizza)>=1:  #client wants to know just one pizza with the ingredient
                         random_pizza = np.random.randint(0, len(list_pizza)) #random int to not always have the first pizzas returned 
                         return {'fulfillmentText': u'La {} ne contient pas les ingrédients {}.'.format(list_pizza[random_pizza], format_list_for_message_client(ingredients))}
 
-                    elif len(list_pizza)==1 and quantity=='pluriel': #just one pizza whereas the client asks for several pizzas
+                    elif len(list_pizza)==1 and quantity[0]=='pluriel': #just one pizza whereas the client asks for several pizzas
                         return {'fulfillmentText': u'Nous avons uniquement la pizza {} sans les ingrédients {}'.format(list_pizza[0], format_list_for_message_client(ingredients))}
-                    elif quantity =='pluriel' and len(list_pizza)>=2:
+                    elif quantity[0] =='pluriel' and len(list_pizza)>=2:
                         return {'fulfillmentText': u'Les pizzas sans l\'ingrédient {} sont {}'.format(format_list_for_message_client(ingredients),format_list_for_message_client(list_pizza))}
-
-            
-
 
 # create a route for webhook
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
-
     return make_response(jsonify(results()))
 
+@app.route('/')
+def route():
+    return("Hello world !")
 # run the app
 if __name__ == '__main__':
     app.run(debug=True)
